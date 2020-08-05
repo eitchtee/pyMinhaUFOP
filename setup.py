23cdd9023ad3d8107ad7865e572ad54b8d65616f
eitchtee <flaips.2@gmail.com>
2020-08-05 14:31:28 -0300

    chore: Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 setuptools.setup(
     name="pyminhaufop",
     packages=['pyminhaufop'],
-    version="0.0.2",
+    version="0.0.3",
     license='MIT',
     author="Herculino Trotta Neto",
     author_email="herculinotrotta@gmail.com",
